cc39c95402c1f70c75b578e3ea9db26dca548f67
JYOTIRMAY25 <jyotirmaybhosale0625@gmail.com>
2026-07-02 15:05:51 +0000

    added variableandcomment program

---
+++ variableandcomment.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a = 10;  // this is an variable
+int b = 2;
+cout<<a+b;
+return 0;
+}
+// this is single line comment
+/* this
+mutil
+line
+comment*/
+
