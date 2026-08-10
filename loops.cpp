daa69fd5ae22bda8a98f87bb41df28fd572c71e7
JYOTIRMAY25 <jyotirmaybhosale0625@gmail.com>
2026-08-10 17:07:11 +0000

    added fliename program

---
+++ loops.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+/* loop in c++
+
+there are total 3 type of loops in c++
+
+1. for loop
+2. while loop
+3. do while loop
+*/
+/*for loop*/
+
+/*for(int i = 0;i<6;i++)
+{
+cout<<i<<endl;
+}
+return 0;
+}
+
+example of infinte loop
+for(int i = 0;5<=8;i++)
+{
+cout<<i<<endl;
+}
+return 0;
+}
+*/
+
+/* while loop */
+/*printing 1 to 40 uisng while loop*/
+/*int i =10;
+while (i<=40){
+cout<<i<<endl;
+i++;
+}
+return 0;
+}
+
+int i =10;
+while (true){
+cout<<i<<endl;
+i++;
+}
+return 0;
+}
+*/
+/* do wile loop */
+int i=1;
+do{
+cout<<i<<endl;
+i++;
+}
+while(false);
+return 0;
+}
